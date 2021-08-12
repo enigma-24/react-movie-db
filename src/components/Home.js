7b4968f0b10763f56acbdb6f1764ae88971b1ec3
Aniket D <aniket.d2402@outlook.com>
2021-08-11 21:51:39 -0400

    use custom hook for home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
-// API
-import API from '../API';
-
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // components
 
 // hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = '') => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-
-      setState((prev) => ({
-        ...movies,
-        results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }));
-    } catch (error) {
-      setError(true);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchMovies(1, '');
-  }, []);
-
+  const { state, loading, error } = useHomeFetch();
   console.log('state ', state);
   return <div>Home Page</div>;
 };
