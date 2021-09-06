89b52aba910fbb8fd7aa072e4707545220734cf0
Aniket D <aniket.d2402@outlook.com>
2021-09-05 21:33:13 -0400

    make api request when search term is entered

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -11,6 +11,7 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -42,5 +43,6 @@ export const useHomeFetch = () => {
     state,
     loading,
     error,
+    setSearchTerm,
   };
 };
