a26a6bd3bf4497658008395137bc712bf9f263e4
Aniket D <aniket.d2402@outlook.com>
2021-09-07 22:08:17 -0400

    show load more button on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 
 // hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -44,7 +45,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner />
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text='Load More' />
+      )}
     </Fragment>
   );
 };
