ff0fb04fb4cd0fdbdaa6acc24b7ce64647b2bfee
Aniket D <aniket.d2402@outlook.com>
2021-09-13 21:12:06 -0400

    added routing

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 // Styles
 import { Image } from './Thumb.styles';
 
-const Thumb = ({image, movieId, clickable}) => (
-    <div>
+const Thumb = ({ image, movieId, clickable }) => (
+  <div>
+    {clickable ? (
+      <Link to={`/${movieId}`}>
         <Image src={image} alt='movie-thumbnail' />
-    </div>
+      </Link>
+    ) : (
+      <Image src={image} alt='movie-thumbnail' />
+    )}
+  </div>
 );
 
 export default Thumb;
