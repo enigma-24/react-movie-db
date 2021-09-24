f59071846e791d88b50d43352c99729002f87ec4
Aniket D <aniket.d2402@outlook.com>
2021-09-23 21:37:19 -0400

    helper file changes

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,14 @@
 // Convert time to hours and minutes
-export const calcTime = time => {
+export const calcTime = (time) => {
   const hours = Math.floor(time / 60);
   const mins = time % 60;
   return `${hours}h ${mins}m`;
 };
 // Convert a number to money formatting
-export const convertMoney = money => {
-  const formatter = new Intl.NumberFormat('en-US', {
+export const convertMoney = (money) => {
+  const formatter = new Intl.NumberFormat('en-CA', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'CAD',
     minimumFractionDigits: 0,
   });
   return formatter.format(money);
