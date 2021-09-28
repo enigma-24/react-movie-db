9dbe4db43e718f33b79bb60702b00f72bd8ff2a2
Aniket D <aniket.d2402@outlook.com>
2021-09-27 21:43:46 -0400

    Actor info page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 import BreadCrumb from './BreadCrumb';
 import { useMovieFetch } from '../hooks/useMovieFetch';
 import NoImage from '../images/no_image.jpg';
@@ -25,6 +26,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header='Actors'>
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
